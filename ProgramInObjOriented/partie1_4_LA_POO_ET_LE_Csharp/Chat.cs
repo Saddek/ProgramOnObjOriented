6b08547960b24fe9301cc679253c0e6144c03a01
Saddek KADJI <ms.kadji@gmail.com>
2016-05-22 14:04:04 +0200

    modif

--- ProgramInObjOriented/partie1_4_LA_POO_ET_LE_Csharp/Chat.cs
+++ ProgramInObjOriented/partie1_4_LA_POO_ET_LE_Csharp/Chat.cs
@@ -8,6 +8,11 @@ namespace ProgramInObjOriented.partie1_4_LA_POO_ET_LE_Csharp
 {
     public class Chat : Animal
     {
+        public Chat() 
+        {
+            Console.WriteLine("Je la classe Chien qui herite de la classe Animal");
+            //this.prenom = prenomDuChat;
+        }
         public Chat(string prenomDuChat) : base (prenomDuChat)
         {
             Console.WriteLine("Je la classe Chien qui herite de la classe Animal");
--- ProgramInObjOriented/partie1_4_LA_POO_ET_LE_Csharp/Program_P1_4.cs
+++ ProgramInObjOriented/partie1_4_LA_POO_ET_LE_Csharp/Program_P1_4.cs
@@ -62,7 +62,71 @@ namespace ProgramInObjOriented.partie1_4_LA_POO_ET_LE_Csharp
 
             Chien chien = new Chien();
             chien.Manger();
+            // CASTING
+            Chien medor = new Chien();
+            Animal animalCasted = (Animal)medor;
+            List<Animal> animaux3 = new List<Animal>();
+            Chien chien3 = new Chien();
+            Chat chat3 = new Chat();
 
+            animaux.Add((Animal)chien3);
+            animaux.Add((Animal)chat3);
+
+            // Erreur
+            /*foreach (Chien c in animaux)
+            {
+                c.Aboyer();
+            }*/
+
+            //Mieux
+
+            foreach (Animal animal in animaux)
+            {
+                if (animal is Chien)
+                {
+                    Chien c = (Chien)animal;
+                    c.Aboyer();
+                }
+                if (animal is Chat)
+                {
+                    Chat c = (Chat)animal;
+                    c.Miauler();
+                }
+            }
+
+            // DYNAMIC CASTING with as
+            foreach (Animal animal in animaux)
+            {
+                Chien c1 = animal as Chien;
+                if (c1 != null)
+                {
+                    c1.Aboyer();
+                }
+                Chat c2 = animal as Chat;
+                if (c2 != null)
+                {
+                    c2.Miauler();
+                }
+            }
+
+            // BOXING : Covertir type valeur en type reference
+            int i = 5;
+            object o = i; // boxing
+            Console.WriteLine("object oGetType  : " + o.GetType());
+
+            int j = 5;
+            object Oo = j; // boxing
+            Oo = 6;
+            j = 10;
+            Console.WriteLine("j = " + j);
+            Console.WriteLine("Oo = " + Oo);
+            // UNBOXING
+
+            int h = 5;
+            object ob = i; // boxing
+            int g = (int)ob; // unboxing
+
+            
             Thread.Sleep(100000);
         }
     }
